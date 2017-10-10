bd6c3f13ed6a14a9c0b07c5d21d906af9c970c8e
Unknown <d228214@edu.rocwb.nl>
2017-10-10 14:37:27 +0200

    Changed login routing
    Changed login routing

--- app/Http/Controllers/pagesController.php
+++ app/Http/Controllers/pagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class pagesController extends Controller
 {
@@ -12,12 +13,16 @@ class pagesController extends Controller
     }
 
     public function Home(){
-        if (Auth::user()->name == "Sales"){
-            return view('customers/clients');
-        }else if(Auth::user()->name == "Finance"){
-            return view('customers/clients');
-        }else if(Auth::user()->name == "Development"){
-            return view('projects/projects');
+        if(Auth::guest()){
+            return view('auth/login');
+        }else{
+            if (Auth::user()['username'] == "Sales"){
+                return redirect('/clients');
+            }else if(Auth::user()['username'] == "Finance"){
+                return redirect('/clients');
+            }else if(Auth::user()['username'] == "Development"){
+                return redirect('/projects');
+            }
         }
     }
 
