0c28faad7b67e7a046ab9f522c77911892bb6341
Unknown <d228214@edu.rocwb.nl>
2017-10-10 09:32:14 +0200

    Restart
    Restart

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
-         $this->call(ProjectsTableSeeder::class);
-         $this->call(project_user_table_seeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
--- resources/views/Development/edit.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: joeyo
- * Date: 04/10/2017
- * Time: 15:26
- */
