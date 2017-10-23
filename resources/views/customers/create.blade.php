7ab311d6326c2cb297a1fe8baa401db40d903762
Unknown <d228214@edu.rocwb.nl>
2017-10-23 09:06:31 +0200

    Changed create.blade clients
    Changed create.blade clients

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -28,45 +28,45 @@
 @section('content')
     <div class="title">
         <h1 id="ptitle">Add Client</h1>
-        @if ( session('success') )
-            <h1> {{session('success')}} </h1>
-        @endif
     </div>
     <div class="wrapper">
         <div class="addclientbox">
+            @if ( session('success') )
+                <p> {{session('success')}} </p>
+            @endif
             <form action="{{ action('clientsController@store') }}" id="addClient" method="post">
                 {{ csrf_field() }}
                 <div class="form-group">
                     <label for="companyNames">Company name</label>
-                    <input type="text" id="companyNames" required>
+                    <input type="text" name="companyNames" id="companyNames" required>
                 </div>
                 <div class="form-group">
-                    <label for="address">Adress</label>
-                    <input type="text" id="adress" required>
+                    <label for="adress">Adress</label>
+                    <input type="text" name="address" id="adress" required>
                 </div>
                 <div class="form-group">
                     <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" required>
+                    <input type="text" name="zipcode" id="zipcode" required>
                 </div>
                 <div class="form-group">
                     <label for="residence">Residence</label>
-                    <input type="text" id="residence" required>
+                    <input type="text" name="residence" id="residence" required>
                 </div>
                 <div class="form-group">
                     <label for="contactperson">Contact person</label>
-                    <input type="text" id="contactperson" required>
+                    <input type="text" name="contactperson" id="contactperson" required>
                 </div>
                 <div class="form-group">
                     <label for="telephonenumber">Telephone number</label>
-                    <input type="text" id="telephonenumber" required>
+                    <input type="text" name="telephonenumber" id="telephonenumber" required>
                 </div>
                 <div class="form-group">
                     <label for="faxnumber">Fax number</label>
-                    <input type="text" id="faxnumber">
+                    <input type="text" name="faxnumber" id="faxnumber">
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="email" id="email" required>
+                    <input type="email" name="email" id="email" required>
                 </div>
                 <div class="form-group">
                     <label for="">Prospect</label>
